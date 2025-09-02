ebcebfd2185068a2d195181c93e0c70f5897b409
shu915 <chukichi915@gmail.com>
2025-09-02 20:53:38 +0900

    ショッププロフィールを閲覧できるようになった

--- app/controllers/api/shops_controller.rb
+++ app/controllers/api/shops_controller.rb
@@ -21,12 +21,12 @@ class Api::ShopsController < ActionController::API
 
   def index
     shops = Shop.all
-    render json: shops_json(shops), status: :ok
+    render json: { shops: shops_json(shops) }, status: :ok
   end
 
   def show
     shop = Shop.find(params[:id])
-    render json: shop_json(shop), status: :ok
+    render json: { shop: shop_json(shop) }, status: :ok
   end
 
   def update
@@ -43,13 +43,8 @@ class Api::ShopsController < ActionController::API
     end
 
     render json: {
-      user:  {
-        id: current_user.id,
-        email: current_user.email,
-        name: current_user.name,
-        role: current_user.role
-      },
-      shop: shop_json(shop)
+        user: user_json(current_user),
+        shop: shop_json(shop)
     }, status: :ok
   end
 
@@ -59,15 +54,22 @@ class Api::ShopsController < ActionController::API
     params.require(:shop).permit(:name, :description, :icon, :header)
   end
 
+  def user_json(user)
+    {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role
+    }
+  end
+
   def shop_json(shop)
     {
-      shop: {
         id: shop.id,
         name: shop.name,
         description: shop.description,
         icon_url:   shop.icon.attached?   ? url_for(shop.icon)   : nil,
         header_url: shop.header.attached? ? url_for(shop.header) : nil
       }
-    }
   end
 end
